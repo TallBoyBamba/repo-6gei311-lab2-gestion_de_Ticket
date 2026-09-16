87586fa9c9e285ac0961ce9fbd7df58c881cf5ca
Almamy99 <asylla29@etu.uqac.ca>
2026-09-16 10:20:07 -0400

    Implementation de la classe Admin

---
+++ Admin.py
@@ -0,0 +1,14 @@
+class Admin :
+    def __init(self, admin_id, name, email ):
+        self.adminId=admin_id
+        self.name=name
+        self.email=email
+        self._tickets=[] # liste des tickets
+    def assignTicket(self,ticket,user):
+        ticket.assignTo(user)# DELEGUE A tickets
+        print(f"{self.name}  a assigne le ticket # {ticket.ticketID}")
+    def closeTicket(self, ticket):
+        ticket.updateStatus ("Termine")# reutilise updates status
+        print(f"{self.name} a ferme le ticket # {ticket.ticketID}")
+    def viewAllTickets(self):
+        return list (self._tickets)# retourne un copie
